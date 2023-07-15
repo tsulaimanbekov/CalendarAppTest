1b4e4d6d41acc760f0f6f480c7a5a7510fb5c933
Tilek Sulaymanbekov <tilek@omega-r.com>
2023-07-15 18:42:38 +0600

    Initial Commit

---
+++ CalendarAppTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalendarAppTest
+//
+//  Created by Tilek Sulaymanbekov on 15/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
